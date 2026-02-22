25f05e27cb483c1496b1c4a16a413f62de0dab2b
Jason Kim <dialogbox@gmail.com>
2026-02-22 14:28:06 +0900

    docs: restructure gh-pages with intro and demo subpage

--- www/vite.config.ts
+++ www/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
 import { execSync } from 'child_process';
+import { resolve } from 'path';
 
 const commitHash = execSync('git rev-parse --short HEAD').toString().trim();
 
@@ -11,6 +12,14 @@ export default defineConfig({
     __COMMIT_HASH__: JSON.stringify(commitHash),
   },
   plugins: [wasm(), topLevelAwait()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        demo: resolve(__dirname, 'demo/index.html'),
+      },
+    },
+  },
   server: {
     fs: {
       // Allow serving files from one level up to the project root
